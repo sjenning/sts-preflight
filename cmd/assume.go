ca2891bfd111c6b71630aa4c0db861779a9defd1
Seth Jennings <sjenning@redhat.com>
2020-10-28 15:48:22 -0500

    implement assume subcommand

--- cmd/assume.go
+++ cmd/assume.go
@@ -2,29 +2,62 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 
+	awssdk "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/sjenning/sts-preflight/pkg/cmd/create"
 	"github.com/spf13/cobra"
 )
 
 // assumeCmd represents the assume command
 var assumeCmd = &cobra.Command{
 	Use:   "assume",
-	Short: "Get STS credentials using an OIDC token (not implemented yet)",
+	Short: "Get STS credentials using an OIDC token",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("assume called")
+		execute()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(assumeCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
 	// assumeCmd.PersistentFlags().String("foo", "", "A help for foo")
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// assumeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func execute() {
+	var state create.State
+	state.Read()
+
+	tokenBytes, err := ioutil.ReadFile("_output/token")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg := &awssdk.Config{
+		Region: awssdk.String(state.Region),
+	}
+
+	s, err := session.NewSession(cfg)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stsClient := sts.New(s)
+
+	output, err := stsClient.AssumeRoleWithWebIdentity(&sts.AssumeRoleWithWebIdentityInput{
+		RoleArn:          awssdk.String(state.RoleARN),
+		WebIdentityToken: awssdk.String(string(tokenBytes)),
+		RoleSessionName:  awssdk.String(fmt.Sprintf("%s-installer-session", state.InfraName)),
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("Run these commands to use the STS credentials")
+	fmt.Printf("export AWS_ACCESS_KEY_ID=%s\n", *output.Credentials.AccessKeyId)
+	fmt.Printf("export AWS_SECRET_ACCESS_KEY=%s\n", *output.Credentials.SecretAccessKey)
+	fmt.Printf("export AWS_SESSION_TOKEN=%s\n", *output.Credentials.SessionToken)
 }
